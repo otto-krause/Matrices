720105fe335f4853b383e479a3bd47a82eb73de1
AlvaroOttoKrause <86359869+AlvaroOttoKrause@users.noreply.github.com>
2021-11-24 02:04:44 -0300

    Add files via upload

--- 4.c
+++ 4.c
@@ -1,28 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int num[3][12],sum=0,sum2=0,i=0,j=0;
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<12;j++)
+    int num[4][13]={0},sum=0,sum2=0,i=0,j=0;
+    do
         {
-            printf("La recaudacion de la linea %d con el colectivo %d es de: \n",i+1,j+1);
+        printf("Ingrese el numero del coche(0-12):\n");
+        scanf("%d",&j);
+        if(j>=1)
+        {
+            printf("Ingrese la linea(1-3):\n");
+            scanf("%d",&i);
+            printf("Ingrese la recaudacion:");
             scanf("%d",&num[i][j]);
         }
-    }
-    system("cls");
-    for(int i=0;i<3;i++)
+        else
+            {
+                break;
+            }
+        }
+    while(j>=1);
     {
-        sum=0;
-        for(int j=0;j<12;j++)
+        for(int i=1;i<=3;i++)
+            {
+                sum=0;
+        for(int j=1;j<=12;j++)
         {
-            printf("La recaudacion de la linea %d con el colectivo %d es de :%d \n",i+1,j+1,num[i][j]);
+            printf("La recaudacion de la linea %d con el colectivo %d es de :%d \n",i,j,num[i][j]);
             sum=sum+num[i][j];
         }
         printf("\n");
         sum2=sum2+sum;
-        printf("La recaudacion de la linea %d es de: %d\n",i+1,sum);
+        printf("La recaudacion de la linea %d es de: %d\n",i,sum);
     }
-    printf("La recaudación total es de: %d",sum2);
+    }
+    printf("La recaudacion total es de: %d",sum2);
     return 0;
 }
